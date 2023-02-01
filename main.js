f31f454436dad9ae9a87ebae9f73040377be294a
NguyenXuanTruong <nxt03091999@gmail.com>
2023-02-01 23:21:30 +0700

    add darkmod code

---
+++ main.js
@@ -0,0 +1,7 @@
+var changeMod = document.querySelector('input')
+var body = document.querySelector('body')
+var changeText = document.querySelector('h4')
+changeMod.addEventListener('click', () =>{
+    body.classList.toggle('dark')
+    changeText.classList.toggle('changetexth4')
+})
