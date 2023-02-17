9b5688780250748ce5dd94f0479e10f43bd11b47
BuuMJ <nxt03091999@gmail.com>
2023-02-17 20:33:38 +0700

    add reponsive code home

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -19,11 +19,13 @@ document.body.onscroll = function() {scrollNavbar()};
 function scrollNavbar() {
 if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 document.getElementById('header').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
+document.getElementById('navbar-mini').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
 document.querySelector('.Courses-show').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
 document.querySelector('.Teachers-show').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
 document.getElementById('moveTopBtn').style.cssText = "opacity: 1; transition: .5s;";
 } else {
 document.getElementById('header').style.cssText = "background-color: #0480FC";
+document.getElementById('navbar-mini').style.cssText = "background-color: #0480FC";
 document.querySelector('.Courses-show').style.cssText = "background-color: #0480FC";
 document.querySelector('.Teachers-show').style.cssText = "background-color: #0480FC";
 document.getElementById('moveTopBtn').style.cssText = "opacity: 0; transition: .5s;";
