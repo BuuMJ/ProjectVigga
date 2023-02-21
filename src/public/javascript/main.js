09951d85954f9220c538ec326fa361e980eb86e1
BuuMJ <nxt03091999@gmail.com>
2023-02-21 17:41:40 +0700

    re-edit all home page

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -15,19 +15,18 @@ function moveToTop() {
 moveToTop();
 
 document.body.onscroll = function() {scrollNavbar()};
-
 function scrollNavbar() {
+  var elems = document.getElementsByClassName('navbar-change');
+  for ( var i = 0; i < elems.length ; i++ )
 if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-document.getElementById('header').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
+document.getElementById('header').style.cssText = "background-color: white; border: none;";
 document.getElementById('navbar-mini').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
-document.querySelector('.Courses-show').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
-document.querySelector('.Users-show').style.cssText = "background-color: rgba(0, 150, 255,0.5)";
+elems[i].style.cssText = "color: black;";
 document.getElementById('moveTopBtn').style.cssText = "opacity: 1; transition: .5s;";
 } else {
-document.getElementById('header').style.cssText = "background-color: #0480FC";
+document.getElementById('header').style.cssText = "background-color: transparent;";
 document.getElementById('navbar-mini').style.cssText = "background-color: #0480FC";
-document.querySelector('.Courses-show').style.cssText = "background-color: #0480FC";
-document.querySelector('.Users-show').style.cssText = "background-color: #0480FC";
+elems[i].style.cssText = "color: white;";
 document.getElementById('moveTopBtn').style.cssText = "opacity: 0; transition: .5s;";
 }
 }
