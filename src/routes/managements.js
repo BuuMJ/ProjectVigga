0da6a9976c2a9bdcf904702a5a5114b99dc5540a
Kocoto <duoc6694@gmail.com>
2023-02-23 19:25:24 +0700

    authorizer

--- src/routes/managements.js
+++ src/routes/managements.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const managementsController = require('../app/controllers/ManagementsController');
+const { checkLogin, checkStaff, checkCoordinator, checkManager, checkAdmin } = require('../ulti/authonize')
 
 router.get('/category' , managementsController.category)
 router.get('/submission' , managementsController.submission)
