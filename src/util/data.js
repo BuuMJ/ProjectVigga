41dc6b8e073eb11a0aba32802090ffa9d3cc3b88
PHU QUY NGUYEN <quynpgcs190822@fpt.edu.vn>
2023-03-10 15:33:16 +0700

    data upload

--- src/util/data.js
+++ src/util/data.js
@@ -1,5 +1,6 @@
 const IdeaModel = require("../app/models/Idea");
 const multer = require("multer");
+const fs = require("fs");
 
 function dataStatistics(req, res, next) {
   IdeaModel.aggregate([
@@ -23,7 +24,13 @@ function dataStatistics(req, res, next) {
 var storage = multer.diskStorage({
   // Thư mục lưu trữ file
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    // Lấy tên submission từ session
+    var submissionName = req.session.idSub.name;
+    // Tạo đường dẫn đầy đủ cho thư mục lưu trữ của submission đó
+    var path = "uploads/" + submissionName + "/";
+    // Tạo thư mục nếu chưa tồn tại
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
   },
   // Đặt tên file
   filename: function (req, file, cb) {
@@ -32,5 +39,17 @@ var storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+// var storage = multer.diskStorage({
+//   // Thư mục lưu trữ file
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   // Đặt tên file
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
 
 module.exports = { dataStatistics, upload };
