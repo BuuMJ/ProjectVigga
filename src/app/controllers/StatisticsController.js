dc575ec1b3735b2ef9b7b3a201077a7a8ad2b367
BuuMJ <nxt03091999@gmail.com>
2023-04-12 19:46:52 +0700

    redesign statistics page

--- src/app/controllers/StatisticsController.js
+++ src/app/controllers/StatisticsController.js
@@ -1,11 +1,15 @@
+const Idea = require('../models/Idea')
+
 class StatisticsController {
   // [GET] /statistic
-  statistic(req, res, next) {
+  async statistic(req, res, next) {
     // console.log(req.department)
+    const count = await Idea.countDocuments({})
     res.render("statistics", {
       title: "Statistics",
       ideaCounts: req.department,
       user: req.user,
+      count: count,
     });
   }
 }
