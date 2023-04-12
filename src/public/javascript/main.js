31c7f22eddcc2d69326e241eab70ae52815befff
BuuMJ <nxt03091999@gmail.com>
2023-04-12 14:39:58 +0700

    complete redesign staffsubmission page

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -19,7 +19,7 @@ function scrollNavbar() {
   var elems = document.getElementsByClassName('navbar-change');
   for ( var i = 0; i < elems.length ; i++ )
 if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-document.getElementById('header').style.cssText = "background-color: white; border: none;";
+document.getElementById('header').style.cssText = "background-color: white; border: none;box-shadow: 2px 2px 8px rgba(0,0,0,0.3);";
 document.getElementById('navbar-mini').style.cssText = "background-color: #232223; border: none;";
 document.getElementById('navbar-icon').style.color = "#e10d11"
 elems[i].style.cssText = "color: black;";
